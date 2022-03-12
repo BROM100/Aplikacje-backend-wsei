555d58abdcbe3a12b1d7236a01c6e0264efb2dd4
Bartlomiej Antos <bartlomiej.antos@microsoft.wsei.edu.pl>
2022-03-12 13:44:48 +0300

    second version

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("file.txt");
+        int lenght = (int) file.length();
+
+        try {
+            FileInputStream stream = new FileInputStream("file.txt");
+            InputStreamReader reader = new InputStreamReader(stream);
+
+            char[] data = new char[lenght];
+            int readBytes = reader.read(data, 0, lenght);
+
+            if (readBytes != lenght) {
+                throw new IOException("File reading error (expected" + lenght + "bytes");
+            }
+            String text = new String(data);
+            System.out.println(text);
+
+            reader.close();
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    }
+
+
